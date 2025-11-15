6d436f7b3e8e4f06dfcad9c249e103261069bbac
Ag teach <agteach5@gmail.com>
2025-11-15 17:16:28 +0300

    first e-commerce commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {  } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+
+
+
+ const App = () => {
+  return (
+    
+    <h1 className="text-4xl text-red-500 font-bold">Hello Tailwind</h1>
+
+    
+  )
+}
+
+export default App
