e5dffe1fd9128d442d6f8fc5f1b7aee59eec196f
Ag teach <agteach5@gmail.com>
2025-11-15 23:30:51 +0300

    first commit for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import {  } from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Cart from './pages/Cart'
 
 
 
 
  const App = () => {
   return (
+    <BrowserRouter>
+     <Navbar/>
+     <Route path="/" element={<Home/>} />
+     <Route path='/products/:id' element={<Products/>} />
+     <Route path='/cart' element={<Cart/>}/>
+
+    </BrowserRouter>
     
-    <h1 className="text-4xl text-red-500 font-bold">Hello Tailwind</h1>
 
     
   )
